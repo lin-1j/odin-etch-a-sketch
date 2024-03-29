3461d14b818d6e529cac78141d01476f46710c29
Jackey2baccey <96613933+Jackey2baccey@users.noreply.github.com>
2024-03-29 12:12:16 +1100

    Add 16x16 grid to the container

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+const GRIDLENGTH = 500;
+let rows = 16;
+let cols = 16;
+
 const container = document.querySelector('.container');
-container.createElement 
+container.style.width = `${GRIDLENGTH}px`;
+container.style.height = `${GRIDLENGTH}px`;
+
+function createGridCells() {
+    for (let i = 0; i < (rows * cols); i++) {
+        let grid = document.createElement('div');
+        grid.style.width = `${GRIDLENGTH / cols - 2}px`;
+        grid.style.height = `${GRIDLENGTH / rows - 2}px`;
+
+        grid.classList.add('grid');
+        container.appendChild(grid);
+    }
+}
+
+createGridCells();
