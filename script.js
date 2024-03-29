45ee7bd9a67a43d6ca3bba16ac03a10a2ac7be80
Jackey2baccey <96613933+Jackey2baccey@users.noreply.github.com>
2024-03-29 12:44:22 +1100

    Add selection functionality which allows users to select grid size

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const GRIDLENGTH = 500;
-let rows = 16;
-let cols = 16;
+let numSquares = 16;
 
 const container = document.querySelector('.container');
 container.style.width = `${GRIDLENGTH}px`;
 container.style.height = `${GRIDLENGTH}px`;
 
+let select = document.querySelector('#select');
+select.addEventListener('click', () => {
+    numSquares = prompt("Enter the number of squares per side: (1-30)");
+
+    while (!(numSquares <= 30 && numSquares >= 1)) {
+        numSquares = prompt("Enter the number of squares per side: (1-30)");
+    }
+
+    createNewGrid();
+})
+
 function createGridCells() {
-    for (let i = 0; i < (rows * cols); i++) {
+    for (let i = 0; i < (numSquares * numSquares); i++) {
         let grid = document.createElement('div');
-        grid.style.width = `${GRIDLENGTH / cols - 2}px`;
-        grid.style.height = `${GRIDLENGTH / rows - 2}px`;
+        grid.style.width = `${GRIDLENGTH / numSquares - 2}px`;
+        grid.style.height = `${GRIDLENGTH / numSquares - 2}px`;
 
         grid.classList.add('grid');
         container.appendChild(grid);
@@ -19,8 +29,15 @@ function createGridCells() {
     }
 }
 
-createGridCells();
-
 function changeBackgroundColor() {
     this.style.backgroundColor = 'black';
 }
+
+function createNewGrid() {
+    container.textContent = '';
+    createGridCells();
+}
+
+
+createGridCells();
+
